12b226e01be8a6cbf73f6e6b86a0a70ecd23ebab
Duabugti123 <115349785+Duabugti123@users.noreply.github.com>
2022-10-27 20:11:25 +0500

    Add files via upload

---
+++ LABTASK7/labtask7.java
@@ -0,0 +1,27 @@
+public class labtask7 {
+
+    public static void main(String[] args) {
+        String var = "I wish I was the \"MONSTER\" that you think i am.";
+        System.out.println(var);
+        String var2 = "It\'s alright.";
+        System.out.println(var2);
+        String var3 = "The character \\ is called backslash.";
+        System.out.println(var3);
+
+        // \n New Line
+        // \r Carriage Return
+        // \t Tab
+        // \b Backspace
+        // \f Form Feed
+
+        String var4 = "DUA\nBUGTI";
+        String var5 = "DUA\rBUGTI";
+        String var6 = "Hello\tDUA";
+        String var7 = "DUA\bBUGTI";
+        System.out.println(var4);
+        System.out.println(var5);
+        System.out.println(var6);
+        System.out.println(var7);
+    }
+
+}
